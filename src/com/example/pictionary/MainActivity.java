fd82e5b33fcc4dd19921579c82a2cbad532e4b95
mliu95 <michael.liu95@gmail.com>
2014-04-10 20:10:39 -0400

    Working draw example

---
+++ src/com/example/pictionary/MainActivity.java
@@ -0,0 +1,21 @@
+package com.example.pictionary;
+
+import android.view.View;
+import com.example.pictionary.R;
+import com.example.pictionary.R.layout;
+import com.example.pictionary.R.menu;
+
+import android.os.Bundle;
+import android.app.Activity;
+import android.view.Menu;
+
+public class MainActivity extends Activity {
+    private DrawingView drawView;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        drawView = (DrawingView)findViewById(R.id.drawing);
+    }
+}
