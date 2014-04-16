779fc112dca8891228940fdf9d837bf9e7218186
dasher5 <chriswrms@hotmail.com>
2014-04-16 18:58:02 -0400

    Added a test case

---
+++ src/com/example/pictionary/DrawControllerTest.java
@@ -0,0 +1,12 @@
+package com.example.pictionary;
+
+import junit.framework.TestCase;
+
+public class DrawControllerTest
+    extends TestCase
+{
+    public void setup()
+    {
+        // does setup
+    }
+}
