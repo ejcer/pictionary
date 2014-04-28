96742f90080be0b5a61506b54f2b4ca4e44f7d6d
emcenrue <edward.mcenrue@gmail.com>
2014-04-28 18:43:02 -0400

    sending for build path adjustment again

--- gen/com/example/pictionary/R.java
+++ gen/com/example/pictionary/R.java
@@ -1266,6 +1266,7 @@ containing a value of this type.
         public static final int action_settings=0x7f0a000f;
         public static final int app_name=0x7f0a000d;
         public static final int hello_world=0x7f0a0010;
+        public static final int title_activity_bullshit=0x7f0a0015;
         public static final int title_activity_draw=0x7f0a0011;
         public static final int title_activity_guess=0x7f0a0012;
         public static final int title_activity_main=0x7f0a000e;
